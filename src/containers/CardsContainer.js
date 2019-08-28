4e239bb5af873ea84922c1684e1a24e2149a8c77
Marcin <zasadzkimarcin@gmail.com>
2019-08-28 05:40:35 +0100

    create cards container

---
+++ src/containers/CardsContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import styles from './CardsContainer.module.css';
+
+class CardsContainer extends Component {
+
+    render() {
+        return (
+            <div className={styles.cardContainer}>Hello</div>
+        );
+    }
+
+}
+
+export default CardsContainer;
