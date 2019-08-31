58df335daf4fa92adcc76331f035a9af35cb161f
Marcin <zasadzkimarcin@gmail.com>
2019-08-31 12:16:01 +0100

    add axios and recive data

--- src/containers/CardsContainer.js
+++ src/containers/CardsContainer.js
@@ -1,30 +1,41 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import styles from './CardsContainer.module.css';
 import CardComponent from '../components/CardComponent';
 
 class CardsContainer extends Component {
 
+    state = {
+        cardsData: [],
+    }
+
+    componentDidMount(){
+        const proxyurl = "https://cors-anywhere.herokuapp.com/";
+        const url ="https://search.moonpig.com/api/products?size=20&fq=card_shop_id:1"; 
+        axios.get(proxyurl + url)
+            .then( response => {
+                console.log(response.data.Products[0]);
+                this.setState({cardsData: response.data.Products});
+            })
+            .catch(error=> {
+                console.log(error);
+            });
+    }
+
     render() {
-
-        const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
         return (
             <div className={styles.Container}>
-
-                {arr.map((str) => {
+                {this.state.cardsData.map((card, index) => {
                     return (
                         <CardComponent
-                            cardUrl={"https://marcinzasadzki.com/img/Sedona@2x.webp"}
-                            imgLink={'https://marcinzasadzki.com/img/Sedona@2x.webp'}
-                            imgDesc={'sedona img'}
-                            title={'this is a card ' + str}
+                            cardUrl={card.ProductLink.Href}
+                            imgLink={card.ProductImage.Link.Href}
+                            imgDesc={card.Title}
+                            title={card.Title}
+                            key={index}
                         />
                     )
                 })}
-
-
-
-
             </div>
         );
     }
