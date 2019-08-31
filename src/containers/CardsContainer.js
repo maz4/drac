0d4bf7b690f6206d6fc7e78671644784755b38cf
Marcin <zasadzkimarcin@gmail.com>
2019-08-31 17:37:25 +0100

    add axios and refactor CardsComponent to handle data request

--- src/containers/CardsContainer.js
+++ src/containers/CardsContainer.js
@@ -2,40 +2,48 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import styles from './CardsContainer.module.css';
 import CardComponent from '../components/CardComponent';
+import Spinner from '../components/Spinner';
 
 class CardsContainer extends Component {
 
     state = {
         cardsData: [],
-    }
+        isLoading: true
+    };
 
-    componentDidMount(){
+    componentDidMount() {
         const proxyurl = "https://cors-anywhere.herokuapp.com/";
-        const url ="https://search.moonpig.com/api/products?size=20&fq=card_shop_id:1"; 
+        const url = "https://search.moonpig.com/api/products?size=20&fq=card_shop_id:1";
         axios.get(proxyurl + url)
-            .then( response => {
+            .then(response => {
                 console.log(response.data.Products[0]);
-                this.setState({cardsData: response.data.Products});
+                this.setState({
+                    cardsData: response.data.Products,
+                    isLoading: false
+                });
             })
-            .catch(error=> {
+            .catch(error => {
                 console.log(error);
             });
     }
 
     render() {
-        return (
-            <div className={styles.Container}>
-                {this.state.cardsData.map((card, index) => {
-                    return (
-                        <CardComponent
-                            cardUrl={card.ProductLink.Href}
-                            imgLink={card.ProductImage.Link.Href}
-                            imgDesc={card.Title}
-                            title={card.Title}
-                            key={index}
-                        />
-                    )
-                })}
+
+        const cards = this.state.cardsData.map((card, index) => {
+            return (
+                <CardComponent cardUrl = {card.ProductLink.Href}
+                imgLink = {card.ProductImage.Link.Href}
+                imgDesc = {card.Title}
+                title = {card.Title}
+                key = {index} />
+            );
+        });
+
+        const spinner = <Spinner />
+
+        return ( <
+            div className = {styles.Container}>
+                { this.state.isLoading ? spinner : cards }
             </div>
         );
     }
