582024198980952bfa8c9d899e1e5fe730ef3bca
Marcin <zasadzkimarcin@gmail.com>
2019-09-02 06:20:22 +0100

    create test for CardContainer

---
+++ src/containers/__tests__/CardContainer.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import CardContainer from '../CardContainer';
+
+test('it should render card with title, description and button', () => {
+  const { queryByText, getByAltText } = render(<CardContainer 
+    cardUrl={"http://cardurl.card"}
+    imgLink={"http://imgurl.img"}
+    imgDesc={"A very nice card"}
+    title={"Celebration Card"} />
+  );
+
+  const title = queryByText(/celebration card/i);
+  const description = queryByText(/a very nice card/i);
+  const alt = getByAltText(/img/i);
+  expect(title).toBeDefined();
+  expect(description).toBeDefined();
+  expect(alt).toBeDefined();
+
+});
