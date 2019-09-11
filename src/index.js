6dafb403fad67c86ac004c9855b985814d158ca9
Marcin <zasadzkimarcin@gmail.com>
2019-09-11 06:34:00 +0100

    remove Root.js and swerviceWorker file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router} from 'react-router-dom';
+import {createStore, compose, applyMiddleware} from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
 import './index.css';
-import Root from './containers/Root';
-import * as serviceWorker from './serviceWorker';
+import App from './components/App';
+import reducers from './reducers/reducers';
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
+
+const app = (
+  <Provider store={store} >
+    <Router>
+      <App />
+    </Router>
+  </Provider>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
